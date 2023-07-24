472843f6d21a1b7a00e2c1652a866682aa6928bb
md-toybur-rahman <toyburrahman735@gmail.com>
2023-07-24 12:56:10 +0600

    get a single id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 // const CollegeData = require('./data/CollegeData.json');
 const port = process.env.PORT || 5000;
@@ -38,6 +38,13 @@ async function run() {
         app.get('/info', async (req, res) => {
             const cursor = collegesInfo.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/info/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result = await collegesInfo.find(query).toArray();
             res.send(result);
         })
 
